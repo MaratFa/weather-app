ec0d59fe746706af0adf0485b1107181587a3f1d
MaratFa <evolution99@rambler.ru>
2025-08-18 06:39:18 +0300

    edited

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-import { setLocationObject } from "./dataFunctions.js";
+import { setLocationObject, getHomeLocation } from "./dataFunctions.js";
 import { addSpinner, displayError } from "./domFunctions.js";
 import CurrentLocation from "./CurrentLocation.js";
 const currentLoc = new CurrentLocation();
@@ -7,8 +7,11 @@ const initApp = () => {
   // add listeners
   const geoButton = document.getElementById("getLocation");
   geoButton.addEventListener("click", getGeoWeather);
+  const homeButton = document.getElementById("home");
+  homeButton.addEventListener("click", loadWeather);
   // set up
   // load weather
+  loadWeather();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
@@ -39,8 +42,39 @@ const geoSuccess = (position) => {
   updateDataAndDisplay(currentLoc);
 };
 
+const loadWeather = (event) => {
+  const savedLocation = getHomeLocation();
+  if (!savedLocation && !event) return getGeoWeather();
+  if (!savedLocation && event.type === "click") {
+    displayError(
+      "No Home Location Saved.",
+      "Sorry. Please save your home location first."
+    );
+  } else if (savedLocation && !event) {
+    displayHomeLocationWeather(savedLocation);
+  } else {
+    const homeIcon = document.querySelector(".fa-home");
+    addSpinner(homeIcon);
+    displayHomeLocationWeather(savedLocation);
+  }
+};
+
+const displayHomeLocationWeather = (home) => {
+  if (typeof home === "string") {
+    const locationJson = JSON.parse(home);
+    const myCoordsObj = {
+      lat: locationJson.lat,
+      lon: locationJson.lon,
+      name: locationJson.name,
+      unit: locationJson.unit,
+    };
+    setLocationObject(currentLoc, myCoordsObj);
+    updateDataAndDisplay(currentLoc);
+  }
+};
+
 const updateDataAndDisplay = async (locationObj) => {
+  console.log(locationObj);
   // const weatherJson = await getWeatherFromCoords(locationObj);
   // if (weatherJson) updateDisplay(weatherJson, locationObj);
 };
-
